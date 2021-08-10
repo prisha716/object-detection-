140db05b75b076e46d6a09b9e6af0c36dec666f7
prisha716 <84237696+prisha716@users.noreply.github.com>
2021-08-10 19:55:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 img="";
 status="";
+object=[];
 
 function preload() 
 {
-    img=loadedImage("dog_cat.jpg");
+    img=loadImage("dog_cat.jpg");
 }
 
 function setup()
  {
-    canvas=createCanvs(640,420);
+    canvas=createCanvas(640,420);
     canvas.center();
     objectDetector=ml5.objectDetector("cocossd",modelLoaded);
     document.getElementById("status").innerHTML="Status:Detecting Objects";
@@ -24,11 +25,23 @@ function gotResult(error,results) {
         console.log(error);
     }
     console.log(results);
+    object = results;
 }
 function draw() 
 {
 
 image(img,0,0,640,420);
+if(status != "") {
+    for(i=0; i<objects.length; i++) {
+        document.getElementById("status").innerHTML="Status : Object Detected";
+        fill("red");
+        percent=floor(objects[i].confidence*100 );
+        text(objects[i].label+""+percent+"%",objects[i].x+15,objects[i].y+15);
+        noFill() ;
+        stroke("red");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+}
 fill("#000000");
 text("Dog",45,75);
 noFill();
